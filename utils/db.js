cf15752d173f3f56cadddabfb01ca0d832d5355f
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-12-09 02:21:37 +0300

    mm

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || '127.0.0.1';
+    const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
     const dbURL = `mongodb://${host}:${port}/${database}`;
