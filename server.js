5b1ed6efda46cdec3d041d7a3c5332fa4abafc7a
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-12-11 18:14:25 +0300

    mm

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ server.use(express.json({ limit: '200mb' }));
 loadRoutes(server);
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, 'localhost', () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
   });
