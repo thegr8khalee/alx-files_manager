ed6b048e3d60ba421f7fa6dd1db22a44a18a425c
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-12-11 16:12:15 +0300

    mm

--- utils/db.js
+++ utils/db.js
@@ -77,14 +77,11 @@
 // export const dbClient = new DBClient();
 // export default dbClient;
 
-
- 
-
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || '127.0.0.1';
+    const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
     const dbURL = `mongodb://${host}:${port}/${database}`;
