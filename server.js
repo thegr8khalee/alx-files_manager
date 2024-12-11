463a35371af22e279779e7cd032a03ada52873bc
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-12-11 16:55:09 +0300

    mm

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(routes);
 // Set the port from the environment variable or default to 5000
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, 'localhost', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
